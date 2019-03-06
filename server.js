e24aab6b8f920ba6ed51a8939f392db388c0415c
abhaynarayansingh-mriirs <abhay.806.98@gmail.com>
2019-03-06 06:47:08 +0530

    set start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express= require('express');
 const hbs= require('hbs');
 const fs= require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();//for config
 app.set('app engine','hbs');
 hbs.registerPartials(__dirname + '/views/partials')
@@ -33,4 +34,6 @@ app.get('/about',(req,res)=>{
     
     });
 });
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`app is running now on ${port}`);
+});
